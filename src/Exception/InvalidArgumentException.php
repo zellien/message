03d5d92aa8fd84219abe52653c7f2ada8bfa8c1a
zellien <development.sites@gmail.com>
2020-04-22 16:40:23 +0300

    New exceptions added

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by Bogdan Tereshchenko <development.sites@gmail.com>
+ * Copyright: 2006-2019 Bogdan Tereshchenko
+ * Link: https://zelliengroup.com/
+ * Date: 22.04.2020 16:35
+ */
+
+declare(strict_types=1);
+
+namespace Zellien\Message\Exception;
+
+use RuntimeException as BaseException;
+
+/**
+ * Class InvalidArgumentException
+ * @package Zellien\Message\Exception
+ */
+class InvalidArgumentException extends BaseException implements ExceptionInterface {
+
+}
---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by Bogdan Tereshchenko <development.sites@gmail.com>
+ * Copyright: 2006-2019 Bogdan Tereshchenko
+ * Link: https://zelliengroup.com/
+ * Date: 22.04.2020 16:36
+ */
+
+declare(strict_types=1);
+
+namespace Zellien\Message\Exception;
+
+use RuntimeException as BaseException;
+
+/**
+ * Class RuntimeException
+ * @package Zellien\Message\Exception
+ */
+class RuntimeException extends BaseException implements ExceptionInterface {
+
+}
